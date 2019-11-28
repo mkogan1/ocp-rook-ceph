8343b550fdb63b2892d90d281dd9bbc20441bc85
Mark Kogan <mkogan@redhat.com>
2019-11-28 18:18:47 +0200

    extract instance creation from deploy script

---
+++ ocs-create-pods.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+OC="$(realpath "${OC:-"$(command -v oc)"}")"
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd "$SCRIPT_DIR" || exit 1
+
+# And machinesets for OCS
+OCS_INSTANCE_TYPE="m5.4xlarge"
+OCS_PER_AZ=1
+MACHINESETS=$("$OC" -n openshift-machine-api get machinesets -o custom-columns=name:metadata.name,replicas:spec.replicas --no-headers  | grep ' 1' | grep -v ocs | awk '{ print $1 }')
+for ms in $MACHINESETS; do
+        "$OC" -n openshift-machine-api get "machinesets/$ms" -ojson |\
+        jq --arg inst "$OCS_INSTANCE_TYPE" --arg rep $OCS_PER_AZ '.metadata.name=.metadata.name+"-ocs"|.spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset"=.metadata.name|.spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset"=.metadata.name|.spec.template.metadata.labels."cluster.ocs.openshift.io/openshift-storage" = ""|.spec.template.spec.providerSpec.value.instanceType=$inst|.spec.replicas=($rep|tonumber)|.spec.template.spec.metadata.labels."cluster.ocs.openshift.io/openshift-storage" = ""' |\
+        "$OC" -n openshift-machine-api apply -f -
+done
+"$OC" -n openshift-machine-api get machinesets
+
